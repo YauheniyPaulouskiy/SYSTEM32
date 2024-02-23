d43693be35bfecba3fa7ab8b6bd8e4ef284913be
Pr0stozheka <pavlovskiy.2001@mail.ru>
2024-02-23 21:00:02 +0300

    AddGameMenu

---
+++ Assets/Scripts/Menu/UI/Text/HighlightedButton.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI.Button
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class HighlightedButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [Header ("Text Object")]
+        [SerializeField] private TMP_Text _buttonText;
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _buttonText.fontStyle = FontStyles.Underline;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _buttonText.fontStyle = FontStyles.Normal;
+        }
+    }
+}
